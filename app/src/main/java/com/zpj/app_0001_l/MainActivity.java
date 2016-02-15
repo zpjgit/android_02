da0610a401d51c90f6915babe6285dafa2613383
zpjgit <zpj.love@foxmail.com>
2016-02-16 00:04:21 +0800

    a3, w7 APP_0001_L J 0003

--- app/src/main/java/com/zpj/app_0001_l/MainActivity.java
+++ app/src/main/java/com/zpj/app_0001_l/MainActivity.java
@@ -26,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button) findViewById(R.id.BUTTON);
 
+        HardControl.ledOpen();
+
         checkBoxLed1 = (CheckBox) findViewById(R.id.LED1);
         checkBoxLed2 = (CheckBox) findViewById(R.id.LED2);
         checkBoxLed3 = (CheckBox) findViewById(R.id.LED3);
@@ -34,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
-                HardControl hardControl = new HardControl();
+                //HardControl hardControl = new HardControl();
                 // Perform action on click
                 ledon = !ledon;
                 if (ledon) {
@@ -43,6 +45,10 @@ public class MainActivity extends AppCompatActivity {
                     checkBoxLed2.setChecked(true);
                     checkBoxLed3.setChecked(true);
                     checkBoxLed4.setChecked(true);
+
+                    for (int i = 0; i < 4; i++) {
+                        HardControl.ledCtrl(i, 1);
+                    }
                 }
                 else {
                     button.setText("ALL ON");//否则就设为ALL ON
@@ -50,6 +56,11 @@ public class MainActivity extends AppCompatActivity {
                     checkBoxLed2.setChecked(false);
                     checkBoxLed3.setChecked(false);
                     checkBoxLed4.setChecked(false);
+
+                    for (int i = 0; i < 4; i++) {
+                        HardControl.ledCtrl(i, 0);
+
+                    }
 
                 }
             }
@@ -66,48 +77,48 @@ public class MainActivity extends AppCompatActivity {
                 if (checked) {
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED1 on", Toast.LENGTH_SHORT).show();
-                   // HardControl.ledCtrl(0, 1);
+                    HardControl.ledCtrl(0, 1);
                 }
                 else {
                     // Remove the meat
                     Toast.makeText(getApplicationContext(), "LED1 off", Toast.LENGTH_SHORT).show();
-                   // HardControl.ledCtrl(0, 0);
+                    HardControl.ledCtrl(0, 0);
                 }
                 break;
             case R.id.LED2:
                 if (checked) {
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED2 on", Toast.LENGTH_SHORT).show();
-                   // HardControl.ledCtrl(1, 1);
+                    HardControl.ledCtrl(1, 1);
                 }
                 else {
                     // Remove the meat
                     Toast.makeText(getApplicationContext(), "LED2 off", Toast.LENGTH_SHORT).show();
-                  //  HardControl.ledCtrl(1, 0);
+                    HardControl.ledCtrl(1, 0);
                 }
                 break;
             case R.id.LED3:
                 if (checked) {
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED3 on", Toast.LENGTH_SHORT).show();
-                    //HardControl.ledCtrl(2, 1);
+                    HardControl.ledCtrl(2, 1);
                 }
                 else {
                     // Remove the meat
                     Toast.makeText(getApplicationContext(), "LED3 off", Toast.LENGTH_SHORT).show();
-                   // HardControl.ledCtrl(2, 0);
+                    HardControl.ledCtrl(2, 0);
                 }
                 break;
             case R.id.LED4:
                 if (checked) {
                     // Put some meat on the sandwich
                     Toast.makeText(getApplicationContext(), "LED4 on", Toast.LENGTH_SHORT).show();
-                   // HardControl.ledCtrl(3, 1);
+                    HardControl.ledCtrl(3, 1);
                 }
                 else {
                     // Remove the meat
                     Toast.makeText(getApplicationContext(), "LED4 off", Toast.LENGTH_SHORT).show();
-                    //HardControl.ledCtrl(3, 0);
+                    HardControl.ledCtrl(3, 0);
                 }
                 break;
 
